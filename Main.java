3e6ce84af1d8745301c5493bd7f67f5295c46581
Nassima Achour <46659041+NassimaAchour@users.noreply.github.com>
2019-10-06 10:40:40 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class MyClass {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter name, age and salary");
+
+    // String input
+    String name = myObj.nextLine();
+
+    // Numerical input
+    int age = myObj.nextInt();
+    double salary = myObj.nextDouble();
+
+    // Output input by user
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Salary: " + salary);
+  }
+}
